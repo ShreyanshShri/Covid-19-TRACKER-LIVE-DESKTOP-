4ccebadc30a38b7e4d9493db835590f62d32357b
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-08-19 23:57:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = async function(){
+ 
+
+    var confirm = document.querySelector(".confirm");
+    var active = document.querySelector(".active");
+    var death = document.querySelector(".death");
+    var recover = document.querySelector(".recover");
+    var title = document.querySelector(".title")
+    
+    
+    
+    const res = await fetch('https://covid19.mathdro.id/api')
+        const data = await res.json()
+        confirm.innerHTML = data.confirmed.value;
+        death.innerHTML = data.deaths.value;
+        recover.innerHTML = data.recovered.value;
+    active.innerHTML = parseInt(data.confirmed.value-(data.recovered.value + data.deaths.value));
+    title.innerHTML = "WORLD";
+        }
